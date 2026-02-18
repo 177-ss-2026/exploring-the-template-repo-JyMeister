768a8944d9301638d2e13da4733c6f2d1513c4d1
JyMeisterDev <jay.braun@student.swic.edu>
2026-02-17 19:37:04 -0600

    add birth year calculation

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ const studentStatus = `Am I a student? ${isStudent}`;
 // Print to Console
 console.log(introduction);
 console.log(studentStatus);
+
+const currentYear = 2026;
+const birthYear = currentYear - age;
+console.log(`I was born in the year ${birthYear}.`);
+
+// It comes out as 1997, but I was born in 1996 and will turn 30 in 2026. :)
